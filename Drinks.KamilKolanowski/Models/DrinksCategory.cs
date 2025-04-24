84f096f11d7b459297973c91d364d22144d4c21b
Kamil Kolanowski <tanssive@yahoo.com>
2025-04-25 00:30:49 +0200

    remove unnecessary fields

--- Drinks.KamilKolanowski/Models/DrinksCategory.cs
+++ Drinks.KamilKolanowski/Models/DrinksCategory.cs
@@ -19,10 +19,7 @@ public class Details
 {
     public string idDrink { get; set; }
     public string strDrink { get; set; }
-    public string strDrinkAlternate { get; set; }
-    public string strTags { get; set; }
     public string strCategory { get; set; }
-    public string strIBA { get; set; }
     public string strAlcoholic { get; set; }
     public string strGlass { get; set; }
     public string strInstructions { get; set; }
